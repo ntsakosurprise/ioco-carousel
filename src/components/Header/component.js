406fbcf15a65c1edb150845dd178dae5f204110b
Surprise <info@turtrozz.com>
2021-08-02 11:42:41 +0200

    Adds header html mark to Header component

--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react'
-
+import {Link} from 'react-router-dom'
 
 class Header extends Component{
 
     render(){
 
         return(
-            <header>
-                Hi there, I am the header component
+            <header className="header">
+               <section className="header__brand">
+                   <Link to='/'  className="header__brand--link">
+                   <img src="/img/mtn_logo.png" alt="MTN Logo" className="header__brand--link-logo" />
+                   </Link>
+               </section>
+               <section className="header__primary-nav">
+                   <nav className="header__primary-nav--nav">
+                        <Link to='/store' className="header__primary-nav--nav-link">Store</Link>
+                        <Link to='/products-services' className="header__primary-nav--nav-link">Products & Services</Link>
+                        <Link to='/help-support' className="header__primary-nav--nav-link">Help & Support</Link>
+                        
+
+                   </nav>
+               </section>
+               <section className="header__secondary-nav">
+                   <nav className="header__secondary-nav--nav">
+                        
+                        <Link to='/personal' className="header__secondary-nav--nav-link">Personal</Link>
+
+                   </nav>
+               </section>
             </header>
         )
     }
