0911c3b712709036cfa18a55a9fb415e3feba854
Surprise <info@turtrozz.com>
2021-08-02 16:43:10 +0200

    Updates carousel implementation
    This commit adds the carouselItems state variable as an object of items that should be looped through as a user navigates the carousel items.

--- src/components/Carousel/component.js
+++ src/components/Carousel/component.js
@@ -7,11 +7,54 @@ class Carousel extends Component{
 
         this.state = {
 
-            carouselItems: []
+            carouselItems: [
+                {
+                    media: '/img/carousel_img_1',
+                    text: 'Mobile Internet',
+                    alt: 'Carousel Mobile internet image'
+                },
+                {
+                    media: '/img/carousel_img_2',
+                    text: 'Home Internet',
+                    alt: 'Carousel Home internet image'
+                },
+                {
+                    media: '/img/carousel_img_3',
+                    text: 'Get a device',
+                    alt: 'Carousel Get a device image'
+                },
+                {
+                    media: '/img/carousel_img_4',
+                    text: 'Add a phone-line',
+                    alt: 'Carousel Add a phone-line image'
+                },
+                {
+                    media: '/img/carousel_img_5',
+                    text: 'Upgrade',
+                    alt: 'Carousel Upgrade image'
+                }
+            ]
         }
     }
 
     getCarouselItems = ()=>{
+
+        const {state} = this 
+        const {carouselItems} = state 
+
+        const carouselElements = carouselItems.map((item,i)=>{
+
+            return <section key={i}>
+                     <figure>
+                         <img src={item.media} alt={item.alt} />
+                     </figure>
+                     <div>
+                         <h4>{item.text}</h4>
+                     </div>
+                   </section>
+        })
+
+        return carouselElements
 
     }
 
@@ -20,6 +63,7 @@ class Carousel extends Component{
         return(
             <div>
 
+                {this.getCarouselItems()}
 
             </div>
         )
