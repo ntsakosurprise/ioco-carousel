4429d3a28ed6333f77089f0e44ba64274f95dffc
Surprise <info@turtrozz.com>
2021-08-02 16:21:13 +0200

    Adds carousel initial implementation

---
+++ src/components/Carousel/component.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+
+class Carousel extends Component{
+
+    constructor(){
+
+        this.state = {
+
+            carouselItems: []
+        }
+    }
+
+    getCarouselItems = ()=>{
+
+    }
+
+    render(){
+
+        return(
+            <div>
+
+
+            </div>
+        )
+    }
+}
+
+
+export default Carousel
