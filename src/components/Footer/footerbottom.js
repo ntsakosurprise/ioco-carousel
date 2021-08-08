69718708f1cbc25e1beb1e6e09b6fdf73fbfac47
Surprise <info@turtrozz.com>
2021-08-08 15:30:41 +0200

    Adds ioco footer content just to make this project more lively

---
+++ src/components/Footer/footerbottom.js
@@ -0,0 +1,54 @@
+import React from 'react' 
+
+
+
+
+export default ()=>{
+
+
+    return(
+
+        <div>
+
+            <section className="footer__brand">
+                <img scr="/img/ioco_footer.png" alt="ioco footer logo" />
+                <span>iOCO is the ICT services brand of the EOH Group</span>
+            </section>
+
+            <section className="footer__contact">
+                <h4>Contact us:</h4>
+                <nav>
+                    <a href="tel:+27116078100">
+                    +27 11 607 8100
+                    </a>
+                    <a href="mailto:solve@ioco.tech">
+                    solve@ioco.tech
+                    </a>
+                </nav>
+            </section>
+
+            <section className="footer__business">
+            <h4>EOH BUSINESS PARK</h4>
+
+                <a href="https://www.google.com/maps/place/EOH/@-26.1740667,28.1259228,17z/data=!4m12!1m6!3m5!1s0x1e95121b7622edc3:0x478a1b07bef2abb3!2sEOH!8m2!3d-26.1740667!4d28.1281115!3m4!1s0x1e95121b7622edc3:0x478a1b07bef2abb3!8m2!3d-26.1740667!4d28.1281115">
+                    Gillooly's View,  Osborne Lane, Bedfordview,2007
+                </a>
+                
+        
+            </section>
+            <section className="footer__social">
+               
+                <nav>
+                    <a href="tel:+27116078100">
+                    +27 11 607 8100
+                    </a>
+                    <a href="mailto:solve@ioco.tech">
+                    solve@ioco.tech
+                    </a>
+                </nav>
+            </section>
+            <strong className="clearfix" />
+
+        </div>
+    )
+}
