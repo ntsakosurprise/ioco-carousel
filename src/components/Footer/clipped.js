6c3eb0f79e5fe792c9c3117a6f7dc867ceed3955
Surprise <info@turtrozz.com>
2021-08-09 23:59:51 +0200

    Adds missing class references

--- src/components/Footer/clipped.js
+++ src/components/Footer/clipped.js
@@ -8,10 +8,11 @@ export default ()=>{
 
         <div className="footer__clipped">
 
-            <section>
+            <section  className="footer__clipped--content" >
                 <h3>iOCO</h3>
                 <h3>Thank you.</h3>
             </section>
+            <section className="footer__clipped--overlay" />
 
         </div>
       )
