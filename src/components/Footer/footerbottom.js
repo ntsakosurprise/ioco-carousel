df70bd962e34b5df107f9952d1acd8c4a5e6de75
Surprise <info@turtrozz.com>
2021-08-10 06:29:13 +0200

    Updates navigation links with proper classess

--- src/components/Footer/footerbottom.js
+++ src/components/Footer/footerbottom.js
@@ -28,10 +28,10 @@ export default ()=>{
             </section>
 
             <section className="footer__business footer__tool">
-            <h4 className="footer__contact--title">EOH BUSINESS PARK</h4>
+            <h4 className="footer__business--title">EOH BUSINESS PARK</h4>
 
                 <a href="https://www.google.com/maps/place/EOH/@-26.1740667,28.1259228,17z/data=!4m12!1m6!3m5!1s0x1e95121b7622edc3:0x478a1b07bef2abb3!2sEOH!8m2!3d-26.1740667!4d28.1281115!3m4!1s0x1e95121b7622edc3:0x478a1b07bef2abb3!8m2!3d-26.1740667!4d28.1281115"
-                className="footer__contact--link">
+                className="footer__business--link">
                     Gillooly's View,  Osborne Lane, Bedfordview,2007
                 </a>
                 
@@ -40,10 +40,10 @@ export default ()=>{
             <section className="footer__social footer__tool">
                
                 <nav>
-                    <a href="tel:+27116078100" className="footer__contact--link">
+                    <a href="tel:+27116078100" className="footer__business--link">
                     +27 11 607 8100
                     </a>
-                    <a href="mailto:solve@ioco.tech" className="footer__contact--link">
+                    <a href="mailto:solve@ioco.tech" className="footer__business--link">
                     solve@ioco.tech
                     </a>
                 </nav>
